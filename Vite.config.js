81fde7e3280318c3736dcc79fa21eb80f5d6118a
yogesh-kadel000 <yoges92457@gmail.com>
2026-09-17 21:18:40 +0545

    Add Vite configuration for server and preview

---
+++ Vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  server: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
+});
